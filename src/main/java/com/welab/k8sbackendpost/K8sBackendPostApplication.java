6be08b2518d524be1821dd167e2f74d1f70b63d6
SuHyunKKim <kimsteven728@dgu.ac.kr>
2025-06-16 20:31:01 +0900

    [feat]: k8s probe add

--- src/main/java/com/welab/k8sbackendpost/K8sBackendPostApplication.java
+++ src/main/java/com/welab/k8sbackendpost/K8sBackendPostApplication.java
@@ -2,8 +2,10 @@ package com.welab.k8sbackendpost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class K8sBackendPostApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/welab/k8sbackendpost/api/backend/BackendK8sController.java
@@ -0,0 +1,35 @@
+package com.welab.k8sbackendpost.api.backend;
+
+import com.welab.k8sbackendpost.common.dto.ApiResponseDto;
+import com.welab.k8sbackendpost.service.probe.ProbeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/backend/post/v1/k8s", produces = MediaType.APPLICATION_JSON_VALUE)
+public class BackendK8sController {
+
+    private final ProbeService probeService;
+
+    @GetMapping(value = "/liveness")
+    public ApiResponseDto<String> liveness() {
+
+        probeService.validateLiveness();
+
+        return ApiResponseDto.defaultOk();
+    }
+
+    @GetMapping(value = "/readiness")
+    public ApiResponseDto<String> readiness() {
+
+        probeService.validateReadiness();
+
+        return ApiResponseDto.defaultOk();
+    }
+}
